8ac420226c84383aa9f9ea933f5ff5191aa870f9
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-05-22 18:38:50 -0500

    Makefile added to run the project

--- Universities/PascualBravo/Programming/2022-05-22/main.cpp
+++ Universities/PascualBravo/Programming/2022-05-22/main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 
+using namespace std;
+
 #include "utils.h"
 
 int main(int argc, char const *argv[]) {
+    bool isRunning = true;
+    int optionSelected = 0;
+    if (!isdigit(2)) {
+        LOG("Is digit");
+    }
+
     LOG(addTwoNumbers(2, 3));
+    while (isRunning) {
+        printMainMenu();
+        cin >> optionSelected;
+
+        switch (optionSelected) {
+            case 1:
+                LOG("Number 1");
+                break;
+            case 2:
+                LOG("Number 1");
+                break;
+            case 3:
+                LOG("Number 1");
+                break;
+            case 4:
+                LOG("Number 1");
+                break;
+            case 5:
+                isRunning = false;
+                break;
+            default:
+                LOG("Por favor selecciona la opción correcta");
+                break;
+        }
+    }
+
     return 0;
 }
--- Universities/PascualBravo/Programming/2022-05-22/utils.h
+++ Universities/PascualBravo/Programming/2022-05-22/utils.h
@@ -3,3 +3,7 @@
 #define LOG(x) std::cout << x << std::endl
 
 int addTwoNumbers(int a, int b);
+
+void printMainMenu();
+
+void printMenuOne();
