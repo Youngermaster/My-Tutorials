d10101cca9b5eab8a8755797201ac0392c88882f
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-05-22 18:38:50 -0500

    Makefile added to run the project

--- Universities/PascualBravo/Programming/2022-05-22/utils.cpp
+++ Universities/PascualBravo/Programming/2022-05-22/utils.cpp
@@ -3,3 +3,19 @@
 int addTwoNumbers(int a, int b) {
     return a + b;
 }
+
+void printMainMenu() {
+    LOG("|===============================|");
+    LOG("|       Proyecto Matemático     |");
+    LOG("|===============================|");
+    LOG("|                               |");
+    LOG("| Selecciona una opción:        |");
+    LOG("|                               |");
+    LOG("| 1. Ejercicios aritméticos.    |");
+    LOG("| 2. Ejercicios de ciclos.      |");
+    LOG("| 3. Ejercicios de cálculos.    |");
+    LOG("| 4. Calcular factorial de N.   |");
+    LOG("| 5. Salir.                     |");
+    LOG("|                               |");
+    LOG("|===============================|");
+}
