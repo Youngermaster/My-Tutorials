c06ec7bd63608090e7ff73483e3cfb094c200adb
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-05-22 19:05:49 -0500

    Factorial added to the project

--- Universities/PascualBravo/Programming/2022-05-22/utils.h
+++ Universities/PascualBravo/Programming/2022-05-22/utils.h
@@ -1,7 +1,13 @@
+#pragma once
 #include <iostream>
+#include <limits>
+
+using namespace std;
 
 #define LOG(x) std::cout << x << std::endl
 
 void printMainMenu();
 
 void printMenuOne();
+
+void factorialOfANumber();
