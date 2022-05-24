5e318c0ea45232ff8f67eb30fbd865d6c54b72e0
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-05-23 20:39:26 -0500

    Binome operations added

--- Universities/PascualBravo/Programming/2022-05-22/utils.cpp
+++ Universities/PascualBravo/Programming/2022-05-22/utils.cpp
@@ -17,19 +17,58 @@ void printMainMenu() {
 }
 
 void printMenuOne() {
-    LOG("|===============================|");
-    LOG("|       Proyecto Matemático     |");
-    LOG("|===============================|");
-    LOG("|                               |");
-    LOG("| Selecciona una opción:        |");
-    LOG("|                               |");
-    LOG("| 1. Ejercicios aritméticos.    |");
-    LOG("| 2. Ejercicios de ciclos.      |");
-    LOG("| 3. Ejercicios de cálculos.    |");
-    LOG("| 4. Calcular factorial de N.   |");
-    LOG("| 5. Salir.                     |");
-    LOG("|                               |");
-    LOG("|===============================|");
+    LOG("|================================================================================|");
+    LOG("|                                                                                |");
+    LOG("| Selecciona una opción:                                                         |");
+    LOG("|                                                                                |");
+    LOG("| 1. Determinar el cuadrado y el cubo de la suma de un binomio.                  |");
+    LOG("| 2. Determinar el cuadrado y el cubo de la diferencia de un binomio.            |");
+    LOG("| 3. Sumar, restar, multiplicar y dividir el inverso de tres números enteros.    |");
+    LOG("| 4. Salir.                                                                      |");
+    LOG("|                                                                                |");
+    LOG("|================================================================================|");
+}
+
+void getSumeBinomeOperations() {
+    int a = 0;
+    int b = 0;
+    LOG("Dame el valor de a");
+    cin >> a;
+
+    LOG("Dame el valor de b");
+    cin >> b;
+
+    if (!cin.good()) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        LOG("Por favor selecciona un número válido.");
+    }
+
+    LOG("El cuadrado de la suma de un binomio es:");
+    LOG(pow((a + b), 2));
+    LOG("El cubo de la suma de un binomio es:");
+    LOG(pow((a + b), 3));
+}
+
+void getMinusBinomeOperations() {
+    int a = 0;
+    int b = 0;
+    LOG("Dame el valor de a");
+    cin >> a;
+
+    LOG("Dame el valor de b");
+    cin >> b;
+
+    if (!cin.good()) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        LOG("Por favor selecciona un número válido.");
+    }
+
+    LOG("El cuadrado de la resta de un binomio es:");
+    LOG(pow((a - b), 2));
+    LOG("El cubo de la resta de un binomio es:");
+    LOG(pow((a - b), 3));
 }
 
 void factorialOfANumber() {
