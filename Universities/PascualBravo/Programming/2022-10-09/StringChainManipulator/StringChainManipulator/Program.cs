022761578fbb72caa7abfe788ee2863e64581f01
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-10-09 20:39:53 -0500

    C# project updated with more functions

--- Universities/PascualBravo/Programming/2022-10-09/StringChainManipulator/StringChainManipulator/Program.cs
+++ Universities/PascualBravo/Programming/2022-10-09/StringChainManipulator/StringChainManipulator/Program.cs
@@ -2,11 +2,30 @@
 
 namespace StringChainManipulator
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            printMainMenu();
+        }
+
+        public static void printMainMenu()
+        {
+            Console.WriteLine("|===============================|");
+            Console.WriteLine("|       Proyecto Matemático     |");
+            Console.WriteLine("|===============================|");
+            Console.WriteLine("|                               |");
+            Console.WriteLine("| Selecciona una opción:        |");
+            Console.WriteLine("|                               |");
+            Console.WriteLine("| 1. Ejercicios aritméticos.    |");
+            Console.WriteLine("| 2. Ejercicios de ciclos.      |");
+            Console.WriteLine("| 3. Ejercicios de cálculos.    |");
+            Console.WriteLine("| 4. Calcular factorial de N.   |");
+            Console.WriteLine("| 5. Salir.                     |");
+            Console.WriteLine("|                               |");
+            Console.WriteLine("|===============================|");
         }
     }
 }
