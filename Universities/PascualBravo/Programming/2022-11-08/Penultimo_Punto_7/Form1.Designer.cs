b7380d4e141c16c09eee53de43ce0c2e6f8982e1
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-11-08 20:52:54 -0500

    Form UX improved

--- Universities/PascualBravo/Programming/2022-11-08/Penultimo_Punto_7/Form1.Designer.cs
+++ Universities/PascualBravo/Programming/2022-11-08/Penultimo_Punto_7/Form1.Designer.cs
@@ -28,39 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label4 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelCategoriaDelEmpleado = new System.Windows.Forms.Label();
+            this.comboBoxCategoria = new System.Windows.Forms.ComboBox();
             this.textBoxCantidadDeHorasTrabajadas = new System.Windows.Forms.TextBox();
             this.textBoxNombreDelTrabajador = new System.Windows.Forms.TextBox();
             this.labelCantidadDeHorasTrabajadas = new System.Windows.Forms.Label();
             this.labelNombreDelTrabajador = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.button_Calcular_Salarios = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.labelResultadoTotal = new System.Windows.Forms.Label();
+            this.buttonCalcularPagoTotal = new System.Windows.Forms.Button();
+            this.buttonAñadirUsuario = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label4
+            // labelCategoriaDelEmpleado
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(680, 83);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(147, 13);
-            this.label4.TabIndex = 15;
-            this.label4.Text = "INGRESE   SU  CATEGORIA";
+            this.labelCategoriaDelEmpleado.AutoSize = true;
+            this.labelCategoriaDelEmpleado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCategoriaDelEmpleado.Location = new System.Drawing.Point(25, 263);
+            this.labelCategoriaDelEmpleado.Name = "labelCategoriaDelEmpleado";
+            this.labelCategoriaDelEmpleado.Size = new System.Drawing.Size(310, 20);
+            this.labelCategoriaDelEmpleado.TabIndex = 15;
+            this.labelCategoriaDelEmpleado.Text = "Seleccione la categoría del Empleado";
             // 
-            // comboBox1
+            // comboBoxCategoria
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.comboBoxCategoria.FormattingEnabled = true;
+            this.comboBoxCategoria.Items.AddRange(new object[] {
             "40000",
             "30000",
             "25000",
             "20000"});
-            this.comboBox1.Location = new System.Drawing.Point(693, 112);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 14;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBoxCategoria.Location = new System.Drawing.Point(30, 304);
+            this.comboBoxCategoria.Name = "comboBoxCategoria";
+            this.comboBoxCategoria.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxCategoria.TabIndex = 14;
+            this.comboBoxCategoria.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // textBoxCantidadDeHorasTrabajadas
             // 
@@ -98,44 +99,49 @@
             this.labelNombreDelTrabajador.Text = "Nombre del Trabajador";
             this.labelNombreDelTrabajador.Click += new System.EventHandler(this.label2_Click);
             // 
-            // label5
+            // labelResultadoTotal
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(680, 360);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(0, 13);
-            this.label5.TabIndex = 16;
+            this.labelResultadoTotal.AutoSize = true;
+            this.labelResultadoTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResultadoTotal.Location = new System.Drawing.Point(534, 167);
+            this.labelResultadoTotal.Name = "labelResultadoTotal";
+            this.labelResultadoTotal.Size = new System.Drawing.Size(345, 55);
+            this.labelResultadoTotal.TabIndex = 16;
+            this.labelResultadoTotal.Text = "Total: 150.000";
+            this.labelResultadoTotal.Click += new System.EventHandler(this.labelResultadoTotal_Click);
             // 
-            // button_Calcular_Salarios
+            // buttonCalcularPagoTotal
             // 
-            this.button_Calcular_Salarios.Location = new System.Drawing.Point(473, 397);
-            this.button_Calcular_Salarios.Name = "button_Calcular_Salarios";
-            this.button_Calcular_Salarios.Size = new System.Drawing.Size(411, 23);
-            this.button_Calcular_Salarios.TabIndex = 17;
-            this.button_Calcular_Salarios.Text = "TOTAL  DE  APORTES  DE  LOS  TRABAJADORES  PARA  IMPUESTOS";
-            this.button_Calcular_Salarios.UseVisualStyleBackColor = true;
-            this.button_Calcular_Salarios.Click += new System.EventHandler(this.button_Calcular_Salarios_Click);
+            this.buttonCalcularPagoTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCalcularPagoTotal.Location = new System.Drawing.Point(544, 372);
+            this.buttonCalcularPagoTotal.Name = "buttonCalcularPagoTotal";
+            this.buttonCalcularPagoTotal.Size = new System.Drawing.Size(416, 48);
+            this.buttonCalcularPagoTotal.TabIndex = 17;
+            this.buttonCalcularPagoTotal.Text = "Calcular Pago Total";
+            this.buttonCalcularPagoTotal.UseVisualStyleBackColor = true;
+            this.buttonCalcularPagoTotal.Click += new System.EventHandler(this.button_Calcular_Salarios_Click);
             // 
-            // button1
+            // buttonAñadirUsuario
             // 
-            this.button1.Location = new System.Drawing.Point(222, 376);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(120, 23);
-            this.button1.TabIndex = 36;
-            this.button1.Text = "Añadir Usuario";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonAñadirUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAñadirUsuario.Location = new System.Drawing.Point(30, 372);
+            this.buttonAñadirUsuario.Name = "buttonAñadirUsuario";
+            this.buttonAñadirUsuario.Size = new System.Drawing.Size(215, 48);
+            this.buttonAñadirUsuario.TabIndex = 36;
+            this.buttonAñadirUsuario.Text = "Añadir Usuario";
+            this.buttonAñadirUsuario.UseVisualStyleBackColor = true;
+            this.buttonAñadirUsuario.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(995, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.button_Calcular_Salarios);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonAñadirUsuario);
+            this.Controls.Add(this.buttonCalcularPagoTotal);
+            this.Controls.Add(this.labelResultadoTotal);
+            this.Controls.Add(this.labelCategoriaDelEmpleado);
+            this.Controls.Add(this.comboBoxCategoria);
             this.Controls.Add(this.textBoxCantidadDeHorasTrabajadas);
             this.Controls.Add(this.textBoxNombreDelTrabajador);
             this.Controls.Add(this.labelCantidadDeHorasTrabajadas);
@@ -150,15 +156,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label labelCategoriaDelEmpleado;
+        private System.Windows.Forms.ComboBox comboBoxCategoria;
         private System.Windows.Forms.TextBox textBoxCantidadDeHorasTrabajadas;
         private System.Windows.Forms.TextBox textBoxNombreDelTrabajador;
         private System.Windows.Forms.Label labelCantidadDeHorasTrabajadas;
         private System.Windows.Forms.Label labelNombreDelTrabajador;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button button_Calcular_Salarios;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelResultadoTotal;
+        private System.Windows.Forms.Button buttonCalcularPagoTotal;
+        private System.Windows.Forms.Button buttonAñadirUsuario;
     }
 }
 
