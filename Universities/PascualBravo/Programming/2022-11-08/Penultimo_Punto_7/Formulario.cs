c09ff48a2152ed2c59716c2e6fce355fd89ea8ea
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-11-09 12:23:52 -0500

    DataGridView bug fixed

---
+++ Universities/PascualBravo/Programming/2022-11-08/Penultimo_Punto_7/Formulario.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Penultimo_Punto_7
+{
+    public partial class Formulario : Form
+    {
+
+        public List<Trabajador> ListaDeTrabajadores = new List<Trabajador>();
+        private int counter = 1;
+
+        private void limpiarFormulario()
+        {
+            textBoxNombreDelTrabajador.Text = "";
+            comboBoxCategoria.Text = "";
+            textBoxCantidadDeHorasTrabajadas.Text = "";
+        }
+
+        private void mostrarDatos()
+        {
+            dataGridViewTrabajadores.DataSource = null;
+            dataGridViewTrabajadores.DataSource = ListaDeTrabajadores;
+            dataGridViewTrabajadores.Update();
+            dataGridViewTrabajadores.Refresh();
+        }
+
+        private void buttonAddUsuarioClick(object sender, EventArgs e)
+        {
+            if (textBoxNombreDelTrabajador.Text != "" && comboBoxCategoria.Text != "" && textBoxCantidadDeHorasTrabajadas.Text != "")
+            {
+                ListaDeTrabajadores.Add(
+                    new Trabajador(
+                        textBoxNombreDelTrabajador.Text,
+                        int.Parse(comboBoxCategoria.Text),
+                        double.Parse(textBoxCantidadDeHorasTrabajadas.Text))
+                    );
+
+                mostrarDatos();
+
+                counter++;
+                labelResultadoTotal.Text = "Añadido " + counter;
+
+                limpiarFormulario();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public Formulario()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Calcular_Salarios_Click(object sender, EventArgs e)
+        {
+        }
+
+
+
+        private void CANT_TRABAJAORES_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void labelResultadoTotal_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Universities/PascualBravo/Programming/2022-11-08/Penultimo_Punto_7/Trabajador.cs
+++ Universities/PascualBravo/Programming/2022-11-08/Penultimo_Punto_7/Trabajador.cs
@@ -6,39 +6,40 @@ using System.Threading.Tasks;
 
 namespace Penultimo_Punto_7
 {
-    internal class Trabajador
+    public class Trabajador
     {
-        public string nombre { get; set; }
-        public double salario { get; set; }
-        public double horasTrabajadas { get; set; }
-        public int categoria { get; set; }
-        public int salarioFinal { get; set; }
+        public string Nombre { get; set; }
+        public double Salario { get; set; }
+        public double HorasTrabajadas { get; set; }
+        public int Categoria { get; set; }
+        public int SalarioFinal { get; set; }
 
 
-        Trabajador(string nombre, int categoria, double horasTrabajadas)
+        public Trabajador(string nombre, int categoria, double horasTrabajadas)
         {
-            this.nombre = nombre;
-            this.categoria = categoria;
-            this.horasTrabajadas = horasTrabajadas;
+            this.Nombre = nombre;
+            this.Categoria = categoria;
+            this.HorasTrabajadas = horasTrabajadas;
         }
+
         public void calcularSalario()
         {
-            switch (this.categoria)
+            switch (this.Categoria)
             {
                 case 1:
-                    this.salario = horasTrabajadas * 40000;
+                    this.Salario = HorasTrabajadas * 40000;
                     break;
                 case 2:
-                    this.salario = horasTrabajadas * 30000;
+                    this.Salario = HorasTrabajadas * 30000;
                     break;
                 case 3:
-                    this.salario = horasTrabajadas * 25000;
+                    this.Salario = HorasTrabajadas * 25000;
                     break;
                 case 4:
-                    this.salario = horasTrabajadas * 20000;
+                    this.Salario = HorasTrabajadas * 20000;
                     break;
                 default:
-                    this.salario = 0;
+                    this.Salario = 0;
                     break;
             }
         }
