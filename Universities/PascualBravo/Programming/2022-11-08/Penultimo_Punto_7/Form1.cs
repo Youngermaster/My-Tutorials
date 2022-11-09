b1e93735420805df6554f3be39e9968e480567e5
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-11-08 20:38:18 -0500

    Windows Forms project added

---
+++ Universities/PascualBravo/Programming/2022-11-08/Penultimo_Punto_7/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Penultimo_Punto_7
+{
+    public partial class Form1 : Form
+    {
+        
+        private List<Trabajador> ListaDeTrabajadores = new List<Trabajador>(); 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("AÑADIR ");
+            this.label5.Text = " Añadio Exitosamente";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private string[] Nombres;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Calcular_Salarios_Click(object sender, EventArgs e)
+        {
+        }
+
+        
+
+        private void CANT_TRABAJAORES_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
---
+++ Universities/PascualBravo/Programming/2022-11-08/Penultimo_Punto_7/Trabajador.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Penultimo_Punto_7
+{
+    internal class Trabajador
+    {
+        public string nombre { get; set; }
+        public double salario { get; set; }
+        public double horasTrabajadas { get; set; }
+        public int categoria { get; set; }
+        public int salarioFinal { get; set; }
+
+
+        Trabajador(string nombre, int categoria, double horasTrabajadas)
+        {
+            this.nombre = nombre;
+            this.categoria = categoria;
+            this.horasTrabajadas = horasTrabajadas;
+        }
+        public void calcularSalario()
+        {
+            switch (this.categoria)
+            {
+                case 1:
+                    this.salario = horasTrabajadas * 40000;
+                    break;
+                case 2:
+                    this.salario = horasTrabajadas * 30000;
+                    break;
+                case 3:
+                    this.salario = horasTrabajadas * 25000;
+                    break;
+                case 4:
+                    this.salario = horasTrabajadas * 20000;
+                    break;
+                default:
+                    this.salario = 0;
+                    break;
+            }
+        }
+
+    }
+}
