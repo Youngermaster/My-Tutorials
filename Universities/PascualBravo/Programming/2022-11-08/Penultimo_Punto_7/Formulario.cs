7f3b015526f41dad4dee1cba7a68b8843ac258ed
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-11-11 10:38:41 -0500

    Windows Forms project commented out

--- Universities/PascualBravo/Programming/2022-11-08/Penultimo_Punto_7/Formulario.cs
+++ Universities/PascualBravo/Programming/2022-11-08/Penultimo_Punto_7/Formulario.cs
@@ -1,68 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Penultimo_Punto_7
 {
     public partial class Formulario : Form
     {
-
+        // En esta lista guardaremos todos los trabajadores que se vayan añadiendo en el formulario.
         public List<Trabajador> ListaDeTrabajadores = new List<Trabajador>();
-
+        // En esta variable iremos guardando los impuestos totales pagados.
         private double pagoTotalDeImpuestos = 0;
+        // En esta variable iremos guardando el total pagado.
         private double pagoTotal = 0;
 
+        public Formulario()
+        {
+            // Inicializa todos los componentes del Windows Forms.
+            InitializeComponent();
+        }
+
+        // Este método limpiará el formulario del Windows Forms para añadir más trabajadores.
         private void limpiarFormulario()
         {
+            // Vacíamos los valores digitados por el usuario.
             textBoxNombreDelTrabajador.Text = "";
-            comboBoxCategoria.Text = "";
             textBoxCantidadDeHorasTrabajadas.Text = "";
+            comboBoxCategoria.Text = "";
         }
 
+        // Este método mostrará la lista de trabajadores en ele Data Grid View.
         private void mostrarDatos()
         {
+            // Reiniciamos el valor de la fuente de datos del DataGridView a vacío.
             dataGridViewTrabajadores.DataSource = null;
+            // Reiniciamos el valor de la fuente de datos del DataGridView a la lista de Trabajadores ya actualizada.
             dataGridViewTrabajadores.DataSource = ListaDeTrabajadores;
-            dataGridViewTrabajadores.Update();
-            dataGridViewTrabajadores.Refresh();
         }
 
+        // Este método se ejecuta cuando presionamos el "botón de añadir Trabajador".
         private void buttonAddUsuarioClick(object sender, EventArgs e)
         {
+            // Primero revisamos que el formulario no tenga ningún valor vacío.
             if (textBoxNombreDelTrabajador.Text != "" && comboBoxCategoria.Text != "" && textBoxCantidadDeHorasTrabajadas.Text != "")
             {
+                // Creamos un trabajador con los valores que el usuario digite.
                 var trabajadorParaAñadir = new Trabajador(
                             textBoxNombreDelTrabajador.Text,
-                            int.Parse(comboBoxCategoria.Text),
-                            double.Parse(textBoxCantidadDeHorasTrabajadas.Text)
+                            int.Parse(comboBoxCategoria.Text), // El "int.Parse()" es para convertir el string a entero.
+                            double.Parse(textBoxCantidadDeHorasTrabajadas.Text) // El "double.Parse()" es para convertir el string a double.
                     );
-
+                // Añadimos el nuevo trabajador creado a la lista de trabajadores.
                 ListaDeTrabajadores.Add(trabajadorParaAñadir);
+                // Acumulamos el valor pagado en impuestos por el trabajador creado.
                 pagoTotalDeImpuestos += trabajadorParaAñadir.impuestosPagados;
+                // Acumulamos el valor total pagado por el trabajador creado.
                 pagoTotal += trabajadorParaAñadir.SalarioFinal + trabajadorParaAñadir.impuestosPagados;
 
+                // Actualizamos el valor de un "Label" para dar la confianza al usuario de que se añadió un nuevo usuario.
                 labelFeedback.Text = "Trabajador añadido.";
 
+                // EJecutamos el método "mostrarDatos()" para actualizar el DataGridView con la nueva lista.
                 mostrarDatos();
+                // EJecutamos el método "limpiarFormulario()" para limpiar el formulario y añadir más usuarios.
                 limpiarFormulario();
             }
         }
 
-        public Formulario()
-        {
-            InitializeComponent();
-        }
-
+        // Este método se ejecuta cuando presionamos el "botón Calcular pago total".
         private void button_Calcular_Salarios_Click(object sender, EventArgs e)
         {
+            // Al texto del Label Resultado Total le actualizamos su valor con el pago total que hemos acumulado.
             labelResultadoTotal.Text = "Total a Pagar: " + pagoTotal.ToString();
+            // Al texto del Label Resultado Total le actualizamos su valor con el pago total de impuestos que hemos acumulado.
             labelTotalImpuestos.Text = "Total a Impuestos: " + pagoTotalDeImpuestos.ToString();
         }
     }
--- Universities/PascualBravo/Programming/2022-11-08/Penultimo_Punto_7/Trabajador.cs
+++ Universities/PascualBravo/Programming/2022-11-08/Penultimo_Punto_7/Trabajador.cs
@@ -1,30 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Penultimo_Punto_7
+﻿namespace Penultimo_Punto_7
 {
     public class Trabajador
     {
+        // El literal { get; set; } nos sirve para definir que una variable sea accesible y editable.
         public string Nombre { get; set; }
-        public double Salario { get; set; }
-        public double HorasTrabajadas { get; set; }
         public int Categoria { get; set; }
+        public double HorasTrabajadas { get; set; }
+        public double Salario { get; set; }
         public double SalarioFinal { get; set; }
         public double impuestosPagados { get; set; }
 
-
+        // Con el siguiente constructor creamos el objeto trabajador.
         public Trabajador(string nombre, int categoria, double horasTrabajadas)
         {
+            // Definimos que el nombre, la categoria y las horasTrabajadas que nos pasen serán los atributos del nuevo Trabajador.
             Nombre = nombre;
             Categoria = categoria;
             HorasTrabajadas = horasTrabajadas;
+            // Llamamos las siguientes 2 funciones para definir el salario y salario final del trabajador.
             calcularSalario();
             calcularSalarioFinal();
         }
 
+        // Este método nos permite calcular el salario del trabajador dependiendo de la categoría que este tenga.
         public void calcularSalario()
         {
             switch (Categoria)
@@ -41,14 +39,18 @@ namespace Penultimo_Punto_7
                 case 4:
                     Salario = HorasTrabajadas * 20000;
                     break;
+                // En caso de que pase algo extraño como un número negativo o alguna otra eventualidad
+                // por buenas prácticas tomamos estos casos problemáticos y los inicializamos a 0.
                 default:
                     Salario = 0;
                     break;
             }
         }
 
+        // Este método nos permite calcular el salario final que recibirá el trabajador.
         public void calcularSalarioFinal()
         {
+            // Definimos las constantes para calcular los impuestos.
             const int SALARIO_LIMITE = 100000;
             const double RETENCION_FUENTE_IMPUESTOS = 5.0 / 100.0;
             const double RETENCION_FUENTE_SEGURIDAD = 3.0 / 100.0;
@@ -56,16 +58,27 @@ namespace Penultimo_Punto_7
 
             if (Salario >= SALARIO_LIMITE)
             {
+                // Si el salario es mayor que el salario límite entonces
+                // los impuestos se calcularán con la retención en la fuente del (5%)
+                // y la retención en la fuente de seguridad (3%).
                 impuestosPagados = (Salario * RETENCION_FUENTE_IMPUESTOS) + (Salario * RETENCION_FUENTE_SEGURIDAD);
+                // Al salario le restamos los impuestos pagados y obtendremos el salario final recibido.
                 SalarioFinal = Salario - impuestosPagados;
             }
             else if (Salario > 0 && Salario < SALARIO_LIMITE)
             {
+                // Si el salario es menor que el salario límite entonces
+                // los impuestos se calcularán con la retención en la fuente de seguridad (3%)
+                // y una auxilio de transporte (80.000).
                 impuestosPagados = (Salario * RETENCION_FUENTE_SEGURIDAD) + AUXILIO_DE_TRANSPORTE;
+                // Al salario le restamos los impuestos pagados, le sumamos el auxilio de
+                // transporte (80.000) y obtendremos el salario final recibido.
                 SalarioFinal = Salario - (Salario * RETENCION_FUENTE_SEGURIDAD) + AUXILIO_DE_TRANSPORTE;
             }
             else
             {
+                // En caso de que pase algo extraño como un número negativo o alguna otra eventualidad
+                // por buenas prácticas tomamos estos casos problemáticos y los inicializamos a 0.
                 impuestosPagados = 0;
                 SalarioFinal = 0;
             }
